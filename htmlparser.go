4d948fd8ca3f67b5133c415d1aabce16d057bf31
Magast Zheng <magast.zheng@morningstar.com>
2014-08-28 18:47:25 +0800

    Add code to parse HTML

--- htmlparser.go
+++ htmlparser.go
@@ -2,6 +2,10 @@
 package main
 
 import(
+    "fmt"
+    "os"
+    "io"
+    "bufio"
     "code.google.com/p/go.net/html"
 )
 
@@ -27,13 +31,37 @@ func (p *StockParser) parseHtml(r io.Reader){
         token := d.Token()
         switch tokenType{
             case html.StartTagToken:
-
+                fmt.Println(d.Data)
             case html.TextToken:
-
+                fmt.Println(d.Data)
             case html.EndTagToken:
-
+                fmt.Println(d.Data)
             case html.SelfClosingTagToken:
-
+                fmt.Println(d.Data)
         }
     }
+}
+
+func (p *StockParser) parseString(html string){
+    r := strings.NewReader(html)
+    p.parseHtml(r)
+}
+
+func main(){
+    filename = "stock_a-ha.dat"
+    f, err := os.Open(filename)
+ 
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+    
+    //content, err := ioutil.ReadFile(filename)
+    //if err != nil {
+    //    fmt.Println("Error to read file!")
+    //}
+
+    reader := bufio.NewReader(f)
+    parser := new(StockParser)
+    parser.parseString(reader)
 }
