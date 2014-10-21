165b0b9566b386069579b548c4b5655885e27776
Magast Zheng <magast.zheng@morningstar.com>
2014-10-21 14:22:01 +0800

    Add the DELETE and UPDATE method for database

--- src/database/stockdb.go
+++ src/database/stockdb.go
@@ -9,8 +9,8 @@ import (
 )
 
 type StockDatabase struct {
-   dbtype string
-   dbcon string
+   Dbtype string
+   Dbcon string
 }
 
 func (s *StockDatabase) CheckError(err error) {
@@ -20,7 +20,7 @@ func (s *StockDatabase) CheckError(err error) {
 }
 
 func (s *StockDatabase) InsertStock(exch string, stock stockhandler.Stock) int {
-    db, err := sql.Open(s.dbtype, s.dbcon)
+    db, err := sql.Open(s.Dbtype, s.Dbcon)
     s.CheckError(err)
 
     stmt, err := db.Prepare("insert stocklist set id=?, name=?, exchange=?, website=?")
@@ -35,6 +35,51 @@ func (s *StockDatabase) InsertStock(exch string, stock stockhandler.Stock) int {
     newid, err := res.LastInsertId()
     s.CheckError(err)
     fmt.Println(newid)
+    
+    db.Close()
+    return 0
+}
 
+func (s *StockDatabase) DeleteStock(stock stockhandler.Stock) int {
+    db, err := sql.Open(s.Dbtype, s.Dbcon)
+    s.CheckError(err)
+
+    stmt, err := db.Prepare("delete from stocklist where id=?")
+    s.CheckError(err)
+    
+    id, err := strconv.Atoi(stock.Id)
+    s.CheckError(err)
+    
+    res, err := stmt.Exec(id)
+    s.CheckError(err)
+    
+    affect, err := res.RowsAffected()
+    s.CheckError(err)
+
+    fmt.Println(affect)
+
+    db.Close()
+    return 0
+}
+
+func (s *StockDatabase) UpdateStock(stock stockhandler.Stock) int {
+    db, err := sql.Open(s.Dbtype, s.Dbcon)
+    s.CheckError(err)
+
+    stmt, err := db.Prepare("update stocklist set name=? where id=?")
+    s.CheckError(err)
+    
+    id, err := strconv.Atoi(stock.Id)
+    s.CheckError(err)
+    
+    res, err := stmt.Exec(stock.Name, id)
+    s.CheckError(err)
+    
+    affect, err := res.RowsAffected()
+    s.CheckError(err)
+
+    fmt.Println(affect)
+
+    db.Close()
     return 0
 }
--- src/database/stockdb_test.go
+++ src/database/stockdb_test.go
@@ -8,14 +8,16 @@ import (
 
 func Test_StockDatabaseInsert(t *testing.T) {
     stdb := StockDatabase{}
-    stdb.dbtype = "mysql"
-    stdb.dbcon = "root@/chinastock"
+    stdb.Dbtype = "mysql"
+    stdb.Dbcon = "root@/chinastock"
 
     stock := stockhandler.Stock{}
     stock.Id = "1234"
     stock.Name = "test"
     stock.Website = "http://www.1234.com"
+    
+    res := stdb.DeleteStock(stock)
 
-    res := stdb.InsertStock("Sh", stock)
+    res = stdb.InsertStock("Sh", stock)
     fmt.Println(res)
 }
