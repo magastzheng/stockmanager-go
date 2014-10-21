51438c15d6613cafd1935ba518a05e9d1e342da6
Magast Zheng <magast.zheng@morningstar.com>
2014-10-21 13:26:57 +0800

    Remove the comment in the configuration

--- src/config/stocklist.go
+++ src/config/stocklist.go
@@ -25,14 +25,6 @@ type StockListConfig struct {
 }
 
 func Parse(filename string) StockListConfig {
-    //file, err := os.Open(filename)
-    //if err != nil {
-    //    panic(err)
-    //}
-    //defer file.Close()
-
-    //chunks := bytes.NewBuffer(nil)
-    //io.Copy(chunks, file)
     chunks, err := ioutil.ReadFile(filename)
     if err != nil {
         panic(err)
