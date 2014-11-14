a24c7367d6a30feba4e71d52e769fd4282fcf73f
Magast Zheng <magast.zheng@morningstar.com>
2014-11-14 14:20:08 +0800

    Update the english name for the account items

--- src/resource/sql/company.sql
+++ src/resource/sql/company.sql
@@ -1,9 +1,13 @@
 use chinastock;
 
-drop table if exists balancesheet;
+/*drop table if exists balancesheet;*/
+drop table if exists bs_currentasset;
 
-create table balancesheet(
-
+create table bs_currentasset(
+    code varchar(10) not null
+    date date not null
+    monetaryfund decimal(15, 2)
+    
 );
 
 
