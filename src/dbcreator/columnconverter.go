1b8d1a0717698dbc847cde87e3f47d20842bef4d
magast <zhengguoq@gmail.com>
2014-11-27 00:27:24 +0800

    Fix the duplicated column converter

--- src/dbcreator/columnconverter.go
+++ src/dbcreator/columnconverter.go
@@ -9,16 +9,7 @@ func ConvertToDBColumn(tabMap map[string][]*acc.Column) []*dbentity.DBTable {
     comDBCols := make([]*dbentity.DBColumn, 0)
     pcols, ok := tabMap[acc.Common]
     if ok {
-        for _, col := range pcols {
-            dbcol := dbentity.DBColumn{
-                Name: col.Column,
-                Type: col.Type,
-                Maxsize: col.Maxsize,
-                IsNotNull: true,
-            }
-
-            comDBCols = append(comDBCols, &dbcol)
-        }
+		comDBCols = ConvertColumn(pcols, true)
     }
     
     dbTabs := make([]*dbentity.DBTable, 0)
@@ -33,19 +24,39 @@ func ConvertToDBColumn(tabMap map[string][]*acc.Column) []*dbentity.DBTable {
         dbTab.Columns = make([]*dbentity.DBColumn, 0)
         dbTab.Keys = make([]*dbentity.DBColumn, 0)
         dbTab.Columns = append(dbTab.Columns, comDBCols ... )
-
-        for _, col := range cols {
-            dbcol := dbentity.DBColumn{
-                Name: col.Column,
-                Type: col.Type,
-                Maxsize: col.Maxsize,
-            }
-
-            dbTab.Columns = append(dbTab.Columns, &dbcol)
-        }
-
+		dbColumns := ConvertColumn(cols, false)
+		dbTab.Columns = append(dbTab.Columns, dbColumns ... )
         dbTabs = append(dbTabs, &dbTab)
     }
 
     return dbTabs
+}
+
+func ConvertColumn(columns []*acc.Column, isnotnull bool) []*dbentity.DBColumn{
+	dbColumns := make([]*dbentity.DBColumn, 0)
+	for _, col := range columns {
+		//don't add duplicated column		
+		if !IsExisted(dbColumns, col.Column) {
+			dbcol := dbentity.DBColumn{
+				Name: col.Column,
+				Type: col.Type,
+				Maxsize: col.Maxsize,
+				IsNotNull: isnotnull,
+			}
+
+			dbColumns = append(dbColumns, &dbcol)
+		}
+	}
+	
+	return dbColumns
+}
+
+func IsExisted(columns []*dbentity.DBColumn, colName string) bool {
+	for _, col := range columns {
+		if col.Name == colName {
+			return true
+		}
+	}
+	
+	return false
 }
