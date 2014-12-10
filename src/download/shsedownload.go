9923ce8781085300c836fa12dff68f2e5e246547
Magast Zheng <magast.zheng@morningstar.com>
2014-12-10 19:06:45 +0800

    Cannot get data of SH SE company

--- src/download/shsedownload.go
+++ src/download/shsedownload.go
@@ -11,15 +11,19 @@ type SHSEDownloader struct {
     config *config.ServiceConfigManager
     listapi config.ServiceAPI
     companyapi config.ServiceAPI
+    certapi config.ServiceAPI
 
     key string
+    qkey string
 }
 
 func (d *SHSEDownloader) Init() {
     d.config = config.NewServiceConfigManager()
     d.key = "shse"
+    d.qkey = "shse-query"
     d.listapi = d.config.GetApi(d.key, "stocklist")
-    d.companyapi = d.config.GetApi(d.key, "company")
+    d.companyapi = d.config.GetApi(d.qkey, "company")
+    d.certapi = d.config.GetApi(d.key, "company-cert")
 }
 
 func (d *SHSEDownloader) GetList() string {
@@ -31,12 +35,15 @@ func (d *SHSEDownloader) GetCompanyInfo(code string) string{
     fmt.Println(d.companyapi.Uri)
     
     rand.Seed(time.Now().UnixNano())
-    randn := rand.Int63()
+    randf := rand.Float32()
+
+    randn := int(randf * (100000000+1))
+
     url := fmt.Sprintf(d.companyapi.Uri, randn, randn, code)
     //return HttpGet(url)
     fmt.Println("after:", url)
     header := make(map[string]string)
-    header["Referer"] = url
+    header["Referer"] = d.certapi.Uri
     return HttpGetWithHeader(url, header)
 }
 
