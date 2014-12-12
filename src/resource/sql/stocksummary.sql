75006e1fbe41c702c9f8d373464e04894aa92105
Magast Zheng <magast.zheng@morningstar.com>
2014-12-12 15:27:18 +0800

    Add table for stocksummary and new stock

---
+++ src/resource/sql/stocksummary.sql
@@ -0,0 +1,42 @@
+--drop database if exists stocksummary;
+--create database stocksummary;
+--use stocksummary;
+use chinastock;
+
+--drop table if exists stocklist;
+--create table stocklist(
+--   id varchar(10) not null primary key,
+--    name varchar(100) not null,
+--    exchange varchar(10) not null,
+--);
+
+drop table if exists newstocklist;
+create table newstocklist(
+	id varchar(10) not null primary key,
+	name varchar(100) not null,
+	exchange varchar(10) not null
+);
+
+drop table if exists stocksummary;
+create table stocksummary(
+	id varchar(10) not null primary key,
+	fullname varchar(100) not null,
+	fullname_en varchar(100),
+	inceptdate date,
+	regaddr varchar(120),
+	addr varchar(120),
+	legalrepresentative varchar(30),
+	boardsecretary varchar(30),
+	email varchar(50),
+	phone varchar(15),
+	website varchar(100),
+	csrccategory varchar(30),
+	csrcbigcategory varchar(30),
+	csrcmidcategory varchar(30),
+	sseindustory varchar(30),
+	city varchar(5),
+	currentstate int,
+	shsample int,
+	inforeign int,
+	foreignaddr varchar(100)
+);
